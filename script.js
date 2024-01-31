7f78174dc65ca738222eaeef92b727ed07d311b3
Dieci1 <pasqualeioio@libero.it>
2024-01-31 16:51:06 +0100

    feat: adds slide function

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//JS of responsive navigation menu
 const menuBtn = document.querySelector(".menu-btn");
 const navigation = document.querySelector(".navigation");
 
@@ -7,3 +8,34 @@ menuBtn.addEventListener("click", () => {
     navigation.classList.toggle("active");
 
 });
+
+//JS for video slider navigation
+const btns = document.querySelectorAll(".nav-btn");
+const slides = document.querySelectorAll(".video-slide");
+const contents = document.querySelectorAll(".content");
+
+var sliderNav = function(manual){
+    btns.forEach((btn) => {
+        btn.classList.remove("active")
+    });
+
+    slides.forEach((slide) => {
+        slide.classList.remove("active")
+    });
+
+    contents.forEach((content) => {
+        content.classList.remove("active")
+    });
+
+
+    btns[manual].classList.add("active");
+    slides[manual].classList.add("active");
+    contents[manual].classList.add("active");
+}
+
+btns.forEach((btn, i) => {
+    btn.addEventListener("click", () => {
+        sliderNav(i);
+    });
+});
+
